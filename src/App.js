6aa8c461d602a1873fa3ae42af8a919024573524
Narendra-2123 <shingannarendra@gmail.com>
2025-01-13 21:01:55 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';  // Import main CSS file
+
+function App() {
+    return (
+        <div className="App">
+            <h1 className="app-heading">Welcome to the Main App</h1>
+            <p className="app-description">This is the main application page.</p>
+        </div>
+    );
+}
+
+export default App;
